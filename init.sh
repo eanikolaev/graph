3b50b691e3516d01e59566d0083de65b04e5e1ec
eanikolaev <evgeny.albertovich@gmail.com>
2014-03-01 22:14:57 +0400

    added script for database creating and filling

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 USER=enikolaev
 BASE=graph
+PSQL="psql $BASE $USER"
 
 psql postgres postgres -c "create user $USER"
 psql postgres postgres -c "create database $BASE owner $USER"
+$PSQL -f init.sql
---
+++ init.sql
@@ -0,0 +1,56 @@
+CREATE TABLE employee (
+    id serial primary key,
+    name varchar(50) not null  
+);
+
+
+CREATE TABLE hierarchy (
+    id integer references employee not null unique,
+    parent integer references employee not null
+);
+
+
+INSERT INTO employee (id, name) VALUES
+    (1, 'Angela Westbrook'),
+    (2, 'Javier Robicheaux'),
+    (3, 'Millard Gillins'),
+    (4, 'Robert Curry'),
+    (5, 'Jordon Gipson'),
+    (6, 'Gregory Stene'),
+    (7, 'Julie Rodgers'),
+    (8, 'Dane Larrow'),
+    (9, 'John Raybuck'),
+    (10, 'Larry Seals'),
+    (11, 'James Riddick'),
+    (12, 'Jeffrey Storey'),
+    (13, 'Mary Jiggetts'),
+    (14, 'Kevin Hyde'),
+    (15, 'Irving Rivera'),
+    (16, 'Margaret Proper'),
+    (17, 'Dale Ahrens'),
+    (18, 'James Owen'),
+    (19, 'Clarita Guerra'),
+    (20, 'Mary Cole');
+
+
+INSERT INTO hierarchy (id, parent) VALUES
+--    (1, -1), 
+    (2,  1),
+    (3,  1),
+    (4,  1),
+    (5,  2),
+    (6,  2),
+    (7,  3),
+    (8,  3),
+    (9,  3),
+    (10, 3),
+    (11, 4),
+    (12, 4),
+    (13, 4),
+    (14, 4),
+    (15, 4),
+    (16, 5),
+    (17, 6),
+    (18, 7),
+    (19, 9),
+    (20, 9);
