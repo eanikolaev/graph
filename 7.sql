8c572c265ade26f7c9e672f60a56722180fdbe84
eanikolaev <evgeny.albertovich@gmail.com>
2014-03-22 11:08:32 +0400

    fixed bug with serial

--- 7.sql
+++ 7.sql
@@ -1,3 +1,16 @@
--- Не понял задания (или его уже сделал на уровне ограничений):
--- 1) двойное подчинение невозможно в силу ограничения unique в колонке id таблицы hierarchy
--- 2) отсутствие руководителя означает, что сотрудник является руководителем отдела
+CREATE OR REPLACE FUNCTION is_tree (
+    head_name varchar(50)
+) RETURNS boolean AS $$
+DECLARE
+    res boolean;
+BEGIN
+    WITH RECURSIVE childs(id, visited, cycle) AS (
+        SELECT employee.id, ARRAY[employee.id], false FROM employee WHERE employee.name=$1
+    UNION ALL
+        SELECT h.id, c.visited || h.id, h.id = ANY(c.visited) FROM hierarchy AS h, childs as c WHERE h.parent=c.id AND NOT c.cycle
+    )
+    SELECT bool_or(childs.cycle) INTO res FROM childs;
+    RETURN res;
+END;
+$$ LANGUAGE plpgsql;
+
