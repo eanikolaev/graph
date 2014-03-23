67e24d770bea107db78ddf1fa42f1aec25978ba9
eanikolaev <evgeny.albertovich@gmail.com>
2014-03-23 20:55:02 +0400

    made 7 task

--- 7.sql
+++ 7.sql
@@ -1,16 +1,7 @@
-CREATE OR REPLACE FUNCTION is_tree (
-    head_name varchar(50)
+CREATE OR REPLACE FUNCTION is_ok (
 ) RETURNS boolean AS $$
-DECLARE
-    res boolean;
 BEGIN
-    WITH RECURSIVE childs(id, visited, cycle) AS (
-        SELECT employee.id, ARRAY[employee.id], false FROM employee WHERE employee.name=$1
-    UNION ALL
-        SELECT h.id, c.visited || h.id, h.id = ANY(c.visited) FROM hierarchy AS h, childs as c WHERE h.parent=c.id AND NOT c.cycle
-    )
-    SELECT bool_or(childs.cycle) INTO res FROM childs;
-    RETURN res;
+    RETURN (SELECT id FROM hierarchy AS h WHERE h.id=h.parent and h.id<>1) is NULL;
 END;
 $$ LANGUAGE plpgsql;
 
